f53323034f30366d05c5fa74ae4e836194aaed1a
Rezahasyim <75386161+Rezahasyim@users.noreply.github.com>
2020-12-15 01:51:37 +0700

    Create Ddos-Attack.py

---
+++ Ddos-Attack.py
@@ -0,0 +1,357 @@
+#!bin/user/python
+
+#decompiled by PDM31
+
+import os, sys
+
+print '\x1b[1;32mMasukkan Pasword Dulu Untuk DdOs Attack'
+
+print '\x1b[1;32mSilahkan Login '
+
+import os, sys
+
+def wa():
+
+    os.system('xdg-open https://api.whatsapp.com/send?phone=+6281221653540
+
+def restart():
+
+    ngulang = sys.executable
+
+    os.execl(ngulang, ngulang, *sys.argv
+
+user = raw_input('ID: ')
+
+import getpass
+
+sandi = raw_input('Password: ')
+
+if sandi == 'bahaya' and user == 'Ddos':
+
+    print 'Anda Telah Login'
+
+    sys.exit
+
+else:
+
+    print 'Login GAGAL, Silahkan hubungi ADMIN'
+
+    wa()
+
+    restart()
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
+
+from multiprocessing.pool import ThreadPool
+
+try:
+import mechanize
+
+except ImportError:
+
+    os.system('pip2 install mechanize')
+
+else:
+
+    try:
+
+        import requests
+
+    except ImportError:
+
+        os.system('pip2 install request')
+
+time.sleep(3)
+
+os.system("clear")
+
+os.system("figlet DdOs")
+
+print
+
+print "Author   :Reza"
+
+print "WA       :081221653540"
+
+print
+
+ip = raw_input("IP Target :")
+port = input("Port       :")
+
+os.system("clear")
+
+os.system("virus starting")
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+
+time.sleep(0.1)
+
+print "Mengirim virus"
+time.sleep(3)
+
+print "Selamat Kamu Kena Prank Kameranya aku taroh di Hatimu😂😘
+
+print "Selamat Belajar Ngoding Broo"
+
+os.system("figlet Godluck")
